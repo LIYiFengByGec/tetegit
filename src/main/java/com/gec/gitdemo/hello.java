91a2da54724b673a7b0f37601fb7f1aeb2d2de83
gec_glb <git_glb@gec.com>
2019-03-07 20:51:24 +0800

    PUT HOT_FIX

--- src/main/java/com/gec/gitdemo/hello.java
+++ src/main/java/com/gec/gitdemo/hello.java
@@ -1,8 +1,11 @@
 package com.gec.gitdemo;
 
+
 public class hello {
 	public static void main(String[] args) {
 		System.out.println("right...");
 		System.out.println("left....");
+		System.out.println("hot_fix");
 	}
 }
+
